8b9506ba2e3422f9e69d7f665ef8ec2ad59885f3
Eduard Sidiropulo <e.sidiropulo@aviata.me>
2020-05-29 15:34:17 +0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+install_requires = [str(ir.req) for ir in parse_requirements('requirements.txt', session=False)]
+
+setup(
+    name='diff-match-patch',
+    version='0.1',
+    description='diff-match-patch',
+    long_description=open('README.md').read().strip(),
+    classifiers=[
+        'Development Status :: 0.1 - Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='diff-match-patch',
+    author='Sidiropulo Eduard',
+    author_email='e.sidiropulo@aviata.me',
+    url='https://github.com/sidiropulos/diff-match-patch.git',
+    license='Other/Proprietary License',
+    install_requires=install_requires,
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
